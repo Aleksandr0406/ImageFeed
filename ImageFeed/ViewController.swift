3716a331acbd3b4224b08dc0820db4b567a47829
Aleksandr <Al3ksandr213@yandex.ru>
2024-10-24 15:10:22 +0300

    Test commit #2

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print("its me")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
+
 
