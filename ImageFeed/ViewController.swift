c60e84fca88551cd11e487b309a7bc59fa60e50a
Aleksandr <Al3ksandr213@yandex.ru>
2024-10-25 18:50:21 +0300

    Настроен лаунчскрин

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("its me")
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
