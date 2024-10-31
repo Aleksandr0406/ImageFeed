f308d30a7ea4d8f54f76fb324d32630414ef874b
Aleksandr <Al3ksandr213@yandex.ru>
2024-11-01 00:12:39 +0300

    Добавил экран пользователя

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by 1111 on 31.10.2024.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var descriptionProfile: UILabel!
+    @IBOutlet weak var mailProfile: UILabel!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var avatarPhoto: UIImageView!
+    
+    @IBAction func exitButton(_ sender: Any) {
+    }
+}
