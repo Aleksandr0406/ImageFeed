e254351050234e7d021c9f68214ba219da52dbfc
Aleksandr <Al3ksandr213@yandex.ru>
2024-11-19 16:05:25 +0300

    Добавил константы  accessKey, secretKey, redirectURI, accessScope, defaultBaseURL

---
+++ ImageFeed/ImagesList/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by 1111 on 19.11.2024.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "NeYGLhAO47Ka5EsgFwTqF4dJzNHyRG_RxAWOn1wwT9M"
+    static let secretKey = "-KvfMLFNlUlAmhhRPasvWKLA7Z8gECAS-tz3jcfO5TY"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "PublicAccess+ReadUserAccess+WriteLikesAccess"
+    static let defaultBaseURL: URL = URL(string: "https://api.unsplash.com/")!
+}
